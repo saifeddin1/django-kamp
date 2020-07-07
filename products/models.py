734d32786e15ef8de379db8792b0b93b38e5624f
saifeddin1 <saifeddinmatoui4@gmail.com>
2020-07-07 20:28:09 +0200

    Add fields price, created_at to Product

--- products/models.py
+++ products/models.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 class Product(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.CharField(max_length=500)
+	price = models.FloatField(null=True)
+	created_at = models.DateTimeField(null=True)
 
 	def get_absolute_url(self):
 		return reverse('product-details', args=(self.id,))
