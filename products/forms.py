284dd82758beec16b42f8c990f203f4595ace878
saifeddin1 <saifeddinmatoui4@gmail.com>
2020-07-08 18:39:15 +0200

    Add templates

---
+++ products/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm 
+from .models import Product
+
+
+class AddProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ('brand', 'title', 'description', 'price',)
+
