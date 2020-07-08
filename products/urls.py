cacc30912865f51ce55e109417b10fe2e4721a64
saifeddin1 <saifeddinmatoui4@gmail.com>
2020-07-08 18:38:54 +0200

    Add Form, product_add, edit and delete views

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('products/', product_list, name="product-list"),
+    path('products/add/', product_add, name="product-add"),
     path('products/<pk>/', product_details, name="product-details"),
+    path('products/edit/<pk>/', product_edit, name="product-edit"),
+    path('products/delete/<pk>/', product_delete, name="product-delete"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from .forms import AddProductForm
 
 
 def product_list(request):
@@ -8,5 +9,39 @@ def product_list(request):
 
 
 def product_details(request, pk):
-	product = get_object_or_404(Product, id=pk)
+	product = get_object_or_404(Product, pk=pk)
 	return render(request, 'products/product_detail.html', {'product':product})
+
+
+def product_add(request):
+	if request.method == 'POST':
+		form = AddProductForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return render(request, 'products/product-add-successful.html')
+	else:
+		
+		form = AddProductForm()
+
+	return render(request, 'products/product-add.html', {'form':form})
+
+
+def product_edit(request, pk):
+	product = get_object_or_404(Product, pk=pk)
+
+	if request.method == 'POST':
+		form = AddProductForm(request.POST, instance=product)
+		if form.is_valid():
+			form.save()
+			return render(request, 'products/product-add-successful.html')
+	else:
+		
+		form = AddProductForm(instance=product)
+
+	return render(request, 'products/product-add.html', {'form':form})
+
+
+def product_delete(request, pk):
+	product = get_object_or_404(Product, pk=pk)
+	product.delete()
+	return redirect('product-list')
