090928fb41d44ead7912b37bf9cea7cb77b04fa3
saifeddin1 <saifeddinmatoui4@gmail.com>
2020-07-12 12:40:26 +0200

    Removed whitespace

--- products/models.py
+++ products/models.py
@@ -3,15 +3,15 @@ from django.urls import reverse
 
 
 class Product(models.Model):
-	brand = models.CharField(max_length=100)
-	title = models.CharField(max_length=100)
-	description = models.CharField(max_length=500)
-	price = models.FloatField(null=True)
-	created_at = models.DateTimeField(null=True, auto_now_add=True)
-	image = models.ImageField(upload_to='products/', null=True)
-	
-	def get_absolute_url(self):
-		return reverse('product-details', args=(self.id,))
+    brand = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.FloatField(null=True)
+    created_at = models.DateTimeField(null=True, auto_now_add=True)
+    image = models.ImageField(upload_to='products/', null=True)
 
-	def __str__(self):
-		return self.title
+    def get_absolute_url(self):
+        return reverse('product-details', args=(self.id,))
+
+    def __str__(self):
+        return self.title
