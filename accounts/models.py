770554e4b368434a657ae52bdc3e7476ab9e1e52
saifeddin1 <saifeddinmatoui4@gmail.com>
2020-07-14 20:34:18 +0200

    Add customer model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.id} - Customer {self.user.username}'
+
+
+def create_customer_profile(sender, instance, created, ** kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+
+post_save.connect(create_customer_profile, sender=User)
