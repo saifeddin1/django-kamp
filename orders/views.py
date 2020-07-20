a709399d06fb134cc3a547d9517fd30843e945f1
saifeddin1 <saifeddinmatoui4@gmail.com>
2020-07-20 11:08:35 +0200

    Switched to Order Model Form

--- orders/views.py
+++ orders/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Order
-from carts.models import Cart
+from django.contrib.auth.decorators import login_required
+
 from .utils import send_success_email
+from.forms import OrderForm
 
 
-# def address(request):
-#     customer = request.user.customer
-#     return render(request, 'orders/address.html', {'customer': customer})
-
-
+@login_required
 def place_order(request):
     user = request.user
-    cart = user.cart
 
-    order = Order.objects.create(user=user)
-    for item in cart.items.all():
-        order.items.add(item)
+    if not user.cart.items.exists():
+        return redirect('product-list')
+
     if request.method == 'POST':
+        form = OrderForm(request.POST, request.FILES)
+        if form.is_valid():
 
-        order.address = request.POST['final_address']
-        order.save()
-        send_success_email(request, user, order)
-        return render(request, 'orders/order_success.html')
-    return render(request, 'orders/order.html', {'customer': user.customer})
+            order = form.save(user)
+            order.address = form.cleaned_data['address']
+            order.save()
+            send_success_email(request, user, order)
+            return render(request, 'orders/order_success.html')
+    else:
+        form = OrderForm()
+
+    context = {'customer': user.customer, 'form': form}
+    return render(request, 'orders/order.html', context)
