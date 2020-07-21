a22e48bfffe8dc0af74b146ed5f0b8ce846ec278
saifeddin1 <saifeddinmatoui4@gmail.com>
2020-07-21 19:15:01 +0200

    Fix db settigns

--- store/production_settings.py
+++ store/production_settings.py
@@ -12,5 +12,5 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 DATABASES = {
-    'default': dj_database_url.config(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
